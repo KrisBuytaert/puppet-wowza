f40645a66f10f0d6bd8f03c2b2bf1e4c11f2ab07
Kristof Keppens <kkeppens@gmail.com>
2012-10-05 11:15:12 +0200

    change wowza package name

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class wowza::params {
 
   case $::operatingsystem {
     /(?i:debian)/: {
-      $wowza_pkg = WowzaMediaServer
+      $wowza_pkg = wowzamediaserver-3.1.2
       $java_pkg = sun-java6-jre
     }
   }
