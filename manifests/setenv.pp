b351e78c19b954a99858caeb22f44cd56e32899a
Kristof Keppens <kkeppens@gmail.com>
2012-10-08 12:44:46 +0200

    setenv class for setting java heap size

---
+++ manifests/setenv.pp
@@ -0,0 +1,13 @@
+class wowza::setenv (
+  $java_heap_size = wowza::params::java_heap_size
+) inherits wowza::params {
+  
+  file { 'setenv.sh':
+    ensure  => 'file',
+    path    => "${wowza::params::installdir}/bin/setenv.sh",
+    owner   => root,
+    group   => root,
+    mode    => '0775',
+    content => template('wowza/setenv.sh.erb'),
+  }
+}
