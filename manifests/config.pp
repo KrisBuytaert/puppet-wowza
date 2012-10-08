10123cd19036780708fb29b24cebf4dae5bff634
Kristof Keppens <kkeppens@gmail.com>
2012-10-08 10:17:58 +0200

    add config class

---
+++ manifests/config.pp
@@ -0,0 +1,24 @@
+class wowza::config (
+  $application,
+  $streamtype = "live",
+  $livestreampacketizers = "cupertinostreamingpacketizer, smoothstreamingpacketizer",
+  $playmethod = "none",
+) inherits wowza::params {
+
+  file { "${wowza::params::installdir}/application/${application}":
+    ensure => 'directory',
+  }
+
+  file {"${wowza::params::installdir}/conf/${application}":
+    ensure => 'directory',
+  }
+
+  file {"Application.xml":
+    path    => "${wowza::params::installdir}/conf/${application}",
+    ensure  => 'file',
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    content => template(wowza/application.xml.erb),
+  }
+}
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,7 @@
 class wowza::params {
 
+  $installdir = '/usr/local/WowzaMediaServer'
+
   case $::operatingsystem {
     /(?i:debian)/: {
       $wowza_pkg = 'wowzamediaserver-3.1.2'
