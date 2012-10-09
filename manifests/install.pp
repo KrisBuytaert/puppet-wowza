0d74b171114bf25e8067c7470af7b275466a9574
Kristof Keppens <kkeppens@gmail.com>
2012-10-09 14:29:28 +0200

    fix folder permissions

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,4 +8,47 @@ class wowza::install {
     [$wowza_pkg, $java_pkg]:
       ensure => 'present';
   }
+
+  #change permissions for the folders
+  file { "${::wowza::installdir}/logs":
+    ensure => 'present',
+    mode   => '775',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { "${::wowza::installdir}/conf":
+    ensure => 'present',
+    mode   => '755',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { "${::wowza::installdir}/applications":
+    ensure => 'present',
+    mode   => '755',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { "${::wowza::installdir}/content":
+    ensure => 'present',
+    mode   => '755',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { "${::wowza::installdir}/keys":
+    ensure => 'present',
+    mode   => '755',
+    owner  => 'root',
+    group  => 'root',
+  }
+  
+  file { "${::wowza::installdir}/transcoder":
+    ensure => 'present',
+    mode   => '755',
+    owner  => 'root',
+    group  => 'root',
+  }
 }
