54b4dec1e30b3a6b72674fa058383242050c5858
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2012-10-10 15:21:34 +0200

    Notify service

--- manifests/configfile.pp
+++ manifests/configfile.pp
@@ -20,6 +20,7 @@ define wowza::configfile (
         mode    => '0644',
         owner   => 'root',
         group   => 'root',
+        notify  => Class['wowza::service'],
     }
   }
 
@@ -32,6 +33,7 @@ define wowza::configfile (
         mode    => '0644',
         owner   => 'root',
         group   => 'root',
+        notify  => Class['wowza::service'],
     }
   }
 }
