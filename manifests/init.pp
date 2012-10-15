ce9bb40bff5efa033adc0b7e918a70a3867a0355
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2012-10-15 15:26:36 +0200

    Add support for collectd monitoring

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,11 @@
 class wowza (
   $wowzakey,
-  $java_heap_size = $wowza::params::java_heap_size,
-  $wowza_pkg = $wowza::params::wowza_pkg
+  $java_heap_size      = $wowza::params::java_heap_size,
+  $wowza_pkg           = $wowza::params::wowza_pkg,
+  $jmxhost             = '',
+  $jmxservice_url      = '',
+  $jmxservice_user     = '',
+  $jmxservice_password = ''
 ) inherits wowza::params {
 
   class {'wowza::install':;} ~>
