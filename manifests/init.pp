4f5dcfe816a5d2f247a12252062092c308cd6d9e
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2012-10-15 15:33:48 +0200

    Use define in collectd class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,7 @@
 class wowza (
   $wowzakey,
   $java_heap_size      = $wowza::params::java_heap_size,
-  $wowza_pkg           = $wowza::params::wowza_pkg,
-  $jmxhost             = '',
-  $jmxservice_url      = '',
-  $jmxservice_user     = '',
-  $jmxservice_password = ''
+  $wowza_pkg           = $wowza::params::wowza_pkg
 ) inherits wowza::params {
 
   class {'wowza::install':;} ~>
