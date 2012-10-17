ab9f3433fc9e77c0f138c230d37f5b24edff0b9f
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2012-10-17 17:06:29 +0200

    Fixed startup at boot.
    Wowza ships with a wrong init file.  The init file that is installed
    works on Debian.

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,9 +3,6 @@ class wowza::service {
   service {
     'WowzaMediaServer':
       ensure    => 'running',
-      # Poorly implemented status command in
-      # Debian init script
-      hasstatus => false,
       enable    => $wowza::enable;
   }
 }
