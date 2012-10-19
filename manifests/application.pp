bbacd25f5472ab0b20d432270e70cee13084bd1b
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2012-10-19 10:09:14 +0200

    Applications are set with a define.

---
+++ manifests/application.pp
@@ -0,0 +1,30 @@
+define wowza::application (
+  $streamtype = 'live',
+  $livestreampacketizers = 'cupertinostreamingpacketizer, smoothstreamingpacketizer',
+  $playmethod = 'none') {
+
+  # Create application folder
+  file { "${wowza::params::installdir}/applications/${name}":
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  # Create application config file and folder
+  file { "${wowza::params::installdir}/conf/${name}":
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755';
+  }
+
+  file { "${wowza::params::installdir}/conf/${name}/Application.xml":
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('wowza/application.xml.erb'),
+    notify  => Service['WowzaMediaServer'];
+  }
+}
--- manifests/config.pp
+++ manifests/config.pp
@@ -1,24 +1,8 @@
 class wowza::config (
-  $application,
-  $streamtype = 'live',
-  $livestreampacketizers = 'cupertinostreamingpacketizer, smoothstreamingpacketizer',
-  $playmethod = 'none'
 ) inherits wowza::params {
 
-  file {
-    "${wowza::params::installdir}/applications/${application}":
-      ensure => 'directory';
-    "${wowza::params::installdir}/conf/${application}":
-      ensure => 'directory';
-    'Application.xml':
-      ensure  => 'file',
-      path    => "${wowza::params::installdir}/conf/${application}/Application.xml",
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      content => template('wowza/application.xml.erb'),
-      notify  => Service['WowzaMediaServer'];
-    '/etc/init.d/WowzaMediaServer':
+  # Place wowza media server init script
+  file { '/etc/init.d/WowzaMediaServer':
       ensure  => present,
       owner   => 'root',
       group   => 'root',
