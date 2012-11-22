03ef4b53b1380d50e6af66e160bfd597f7af0195
Kristof Keppens <kkeppens@gmail.com>
2012-11-22 07:41:34 +0100

    fix typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class wowza (
   $enable               = $wowza::params::enable,
   $java_heap_size       = $wowza::params::java_heap_size,
   $wowza_pkg            = $wowza::params::wowza_pkg,
-  $wowza_pkg_version    = $wowza::params::wowza_pkg_versiont  :q
+  $wowza_pkg_version    = $wowza::params::wowza_pkg_version,
   $loadtest_ensure      = $wowza::params::loadtest_ensure,
   $loadtest_workercount = $wowza::params::loadtest_workercount,
   $loadtest_streamname  = $wowza::params::loadtest_streamname,
