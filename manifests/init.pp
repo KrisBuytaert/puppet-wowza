42494a0c3d778953352f8fda9c5e0c0b3ad5107c
Kristof Keppens <kkeppens@gmail.com>
2013-06-05 13:09:29 +0200

    include config class to include init script

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,6 +14,7 @@ class wowza (
 
   class {'wowza::install':;} ~>
     class {'wowza::serverconfig':;} ~>
+    class {'wowza::config':;} ~>
     class {'wowza::setenv':;} ~>
     class {'wowza::service':;}
 
