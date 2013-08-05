5fa7a6fe990fdbbdaa861e11932e8b5160594ef3
Kristof Keppens <kkeppens@gmail.com>
2013-08-05 13:35:24 +0200

    make it possible to define a different owner and group for an application

--- manifests/application.pp
+++ manifests/application.pp
@@ -4,6 +4,8 @@ define wowza::application (
   $livestreampacketizers = 'cupertinostreamingpacketizer, smoothstreamingpacketizer',
   $playmethod = 'none',
   $rtmp_protect = 'false',
+  $user = 'root',
+  $group = 'root',
   $storagedir = undef,
 ) {
 
@@ -17,8 +19,8 @@ define wowza::application (
   file { "${wowza::params::installdir}/applications/${name}":
     ensure => $dir_ensure,
     force  => true,
-    owner  => 'root',
-    group  => 'root',
+    owner  => $user,
+    group  => $group,
     mode   => '0755',
   }
 
@@ -26,15 +28,15 @@ define wowza::application (
   file { "${wowza::params::installdir}/conf/${name}":
     ensure => $dir_ensure,
     force  => true,
-    owner  => 'root',
-    group  => 'root',
+    owner  => $user,
+    group  => $group,
     mode   => '0755';
   }
 
   file { "${wowza::params::installdir}/conf/${name}/Application.xml":
     ensure  => $ensure,
-    owner   => 'root',
-    group   => 'root',
+    owner   => $user,
+    group   => $group,
     mode    => '0644',
     content => template('wowza/application.xml.erb'),
     notify  => Service['WowzaMediaServer'];
@@ -43,8 +45,8 @@ define wowza::application (
   if $rtmp_protect == "true" {
     file { "${wowza::params::installdir}/conf/${name}/publish.password":
       ensure => $ensure,
-      owner => 'root',
-      group => 'root',
+      owner => $user,
+      group => $group,
       mode => '0644',
       replace => false,
       source => 'puppet:///modules/wowza/publish.password';
